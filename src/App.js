9b0a8b37a46314929d6ec12776e981f614b00715
Kim Jae Hyeok <master_catch@naver.com>
2026-04-09 17:57:41 +0900

    fix : 페이지 연결을 위한 라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import MainPage from './pages/MainPage';
 
+// sub page
+import AboutPage from './pages/sub/AboutPage';
+import DetailPage from './pages/sub/DetailPage';
+
 function App() {
-  return <MainPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/about/:type" element={<DetailPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
