e3638717d90494333ec6bd7cfb339fd343570505
Kim Jae Hyeok <master_catch@naver.com>
2026-04-14 11:09:12 +0900

    fix: 용역사업소개 페이지 app.js 추가

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import Partners from './pages/sub/aboutCompany/Partners';
 import OrderStatus from './pages/sub/news/OrderStatus';
 import SoftkNews from './pages/sub/news/SoftkNews';
 
+import Business from './pages/sub/business/Business';
+
 import Clients from './pages/sub/clients/Clients';
 import Dirctions from './pages/sub/directions/Directions';
 
@@ -28,6 +30,8 @@ function App() {
 
         <Route path="/order-status" element={<OrderStatus />} />
         <Route path="/softk-news" element={<SoftkNews />} />
+        
+        <Route path="/business/:businessId" element={<Business />} />
         
         <Route path="/clients" element={<Clients />} />
         
